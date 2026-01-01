84906e19d56bef367fd607a8c94d2050bf58f6e4
tempestaGui <guilhermeftempesta@gmail.com>
2026-01-01 18:57:59 -0300

    Project created

---
+++ Workshop/src/test/java/com/shop/Workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
